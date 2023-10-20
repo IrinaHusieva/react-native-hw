d8d5a2c0b1f34c82fca484b10234587d46accc71
Iryna Husieva <02guseva02@gmail.com>
2023-10-20 09:44:20 +0200

    hw4

--- App.js
+++ App.js
@@ -1,34 +1,35 @@
+import React from 'react';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-import React from "react";
-import { StyleSheet, View } from "react-native";
-import { StatusBar } from "expo-status-bar";
-import { useFonts } from "expo-font";
-import Navigation from "./src/navigation/navigation";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View } from 'react-native';
 
-const App = () => {
-  const [fontsLoaded] = useFonts({
-    "Roboto-regular": require("./src/assets/fonts/Roboto-Regular.ttf"),
-    "Roboto-medium": require("./src/assets/fonts/Roboto-Medium.ttf"),
-    "Roboto-bold": require("./src/assets/fonts/Roboto-Bold.ttf"),
-  });
+import LoginScreen from './src/Screens/AuthPages/LoginScreen';
+import RegistrationScreen from './src/Screens/AuthPages/RegistrationScreen';
+import Home from './src/Screens/HomePages/Home';
 
-  if (!fontsLoaded) {
-    return null;
-  }
+const MainStack = createStackNavigator();
 
+export default function App() {
   return (
     <View style={styles.container}>
-      <Navigation />
-      <StatusBar style="auto" />
+      <NavigationContainer>
+        <MainStack.Navigator initialRouteName="Login" screenOptions={{ headerShown: false }}>
+          <MainStack.Screen name="Regestration" component={RegistrationScreen} />
+          <MainStack.Screen name="Login" component={LoginScreen} />
+          <MainStack.Screen name="Home" component={Home} />
+        </MainStack.Navigator>
+        {/* <StatusBar style="auto" /> */}
+      </NavigationContainer>
     </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: '#fff',
   },
 });
-
-export default App;
